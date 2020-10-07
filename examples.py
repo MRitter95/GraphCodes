053f6d857b23ca28662853b336d54f5deb42f5e9
Martin Ritter <maritter@umd.edu>
2020-10-07 18:40:46 -0400

    Example driver file

---
+++ examples.py
@@ -0,0 +1,9 @@
+from GraphCodes.examples import *
+
+#Ex_C60.run()
+#Ex_GeneralLayoutTester.run()
+#Ex_HarshExamples.run()
+#Ex_JakeExamples.run()
+#Ex_MathPhysPaperPlots4.run()
+#Ex_SamplePlots_collected.run()
+#Ex_UnitCellTester.run()
