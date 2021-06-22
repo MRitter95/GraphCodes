a50f2f40c5f67dad82144d3a65d04c7f8086afed
Martin Ritter <maritter@umd.edu>
2021-06-22 16:28:15 -0400

    Fixed issues appearing on mac with package paths

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
 import site
 import os
 import sys
+from distutils.sysconfig import get_python_lib
 
-site_directory = site.getusersitepackages()
+#site_directory = site.getusersitepackages()
+#
+#site_directory = r'C:\Users\Kollarlab\Anaconda3\Lib\site-packages'
+
+site_directory = get_python_lib()
+
 package_directory = os.getcwd()
 
-filepath = os.path.join(site_directory+"\kollarlab.pth")
+filepath = os.path.join(site_directory,"graphcodes_kollar.pth")
 
 print(site_directory)
 print(package_directory)
 print(filepath)
 
-f = open(filepath,"w")
-f.write(package_directory)
+#f = open(filepath,"w")
+#f.write(package_directory)
